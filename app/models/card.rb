f11585b3b29e3fe21a4151005aa5431fa166b2c7
Piotr Witek <pio.witek@gmail.com>
2025-05-13 08:19:13 +0200

    Broadcast repace of card on card update

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,10 +10,15 @@ class Card < ApplicationRecord
   delegate :board, to: :board_column
 
   after_create_commit :broadcast_card_created
+  after_update_commit :broadcast_card_updated
 
   private
 
   def broadcast_card_created
     broadcast_append_to board, target: dom_id(board_column, :column_body), partial: "cards/card", locals: { card: self }
+  end
+
+  def broadcast_card_updated
+    broadcast_replace_to board, target: dom_id(self), partial: "cards/card", locals: { card: self }
   end
 end
